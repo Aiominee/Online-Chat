c69b952808f91649d8cd26794a956cc6bf0b3640
Aiominee <162189341+Aiominee@users.noreply.github.com>
2024-09-23 20:09:24 +0500

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,32 @@
+using Chat.Hubs;
+
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddStackExchangeRedisCache(options =>
+{
+    var connection = builder.Configuration.GetConnectionString("Redis");
+    options.Configuration = connection;
+});
+
+
+builder.Services.AddCors(options =>
+{
+    options.AddDefaultPolicy(policy =>
+    {
+        policy.WithOrigins("http://localhost:3000")
+            .AllowAnyHeader()
+            .AllowAnyMethod()
+            .AllowCredentials();
+    });
+});
+
+builder.Services.AddSignalR(options => 
+{ 
+    options.EnableDetailedErrors = true; 
+}); 
+
+var app = builder.Build();
+app.UseCors();
+app.MapHub<ChatHub>("/chat");
+app.Run();
